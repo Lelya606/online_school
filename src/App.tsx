b162e7c31f8573554dbff28298e2192632cc5907
Lelya606 <elenamironova0302@gmail.ru>
2023-05-31 02:08:37 +0300

    feat: add courses module

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HomePage } from 'pages/HomePage';
 import 'styles/globalStyles.scss';
+import 'styles/font.scss';
 
 export const App = () => <HomePage />;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MainWrapper } from 'components/HOC/MainWrapper';
 import { Hero } from 'components/modules/Hero/Hero';
+import { Courses } from 'components/modules/Courses/Courses';
 
 export const HomePage = () => (
   <MainWrapper>
     <Hero />
+    <Courses />
   </MainWrapper>
 );
